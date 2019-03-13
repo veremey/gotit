1a502f84ddeb124d06ac2c7d8fd73e1254fb3031
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-03-13 18:49:28 +0200

    blog hrad path

--- dev/temp/mocksData.js
+++ dev/temp/mocksData.js
@@ -1,6 +1,21 @@
 'about': {},
 
-'blog': {},
+'blog': {
+	item: [{
+		img: "static/img/content/blog/blog-01.png",
+		title: "Ho-ho-ho",
+		date: "01 January 2019",
+		dateTime: "2019-01-01"
+	}, {
+		img: "static/img/content/blog/blog-01.png",
+		title: "Ho-ho-ho",
+		date: "01 January 2019",
+		dateTime: "2019-01-01"
+	}
+]}
+
+
+,
 
 'brush-creek-ranch': {},
 
--- markup/components/blog/data/data.js
+++ markup/components/blog/data/data.js
@@ -1,1 +1,15 @@
-var data = {blog: {}};
+var data = {blog: {
+	item: [{
+		img: "static/img/content/blog/blog-01.png",
+		title: "Ho-ho-ho",
+		date: "01 January 2019",
+		dateTime: "2019-01-01"
+	}, {
+		img: "static/img/content/blog/blog-01.png",
+		title: "Ho-ho-ho",
+		date: "01 January 2019",
+		dateTime: "2019-01-01"
+	}
+]}};
+
+
